0a6d240f22cb70a37474f32a15d75c99a636abd5
WooFiles <122677098+Gopooo@users.noreply.github.com>
2023-07-28 22:28:15 +0200

    Add files via upload

---
+++ Kadow Clubwheel_files/config.js
@@ -0,0 +1,21 @@
+TS_CONFIG = {
+  BACKEND_URL: 'https://api.tootsweet-app.com/v1.11',
+  AUTH_URL: 'https://api.tootsweet-app.com/auth',
+  BOOKINGS_URL: 'https://api.tootsweet-app.com/bookings',
+  IMAGES_URL: 'https://tootsweet.twic.pics',
+  IMAGES_URL_PATH: '',
+  GMAP_API_KEY: 'AIzaSyCdPYea04TyaG1hTy4_1LxEJu2iCSo70ng',
+  FB_APP_ID: '306567376209451',
+  GENERATOR_URL: 'http://62.210.251.238:3001',
+  SITE_ROOT: 'https://tootsweet.app',
+  PUBLIC_FILES_URL: 'https://tootsweet-public.s3.eu-west-3.amazonaws.com',
+  PUBLIC_FILES_URL_CDM:
+    'https://s3.eu-west-3.amazonaws.com/cadeaudelamaison.com',
+  CUSTOMER_LOGO_FILES_URL:
+    'https://s3.eu-west-3.amazonaws.com/cadeaudelamaison.com/customer-logos',
+  CUSTOMER_LOGO_FILES_FOLDER: 'customer-logos',
+  OFFER_GIFT_SHARE_URL: 'https://s.cadeaudelamaison.com',
+  ENABLE_HOTJAR: true,
+  ENABLE_AMPLITUDE: true,
+  ENABLE_SENTRY: true,
+}
